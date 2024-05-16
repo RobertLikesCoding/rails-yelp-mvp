87dda5035f39d46bbaff703d077e79838bf441a7
Robert Baufeld <rbaufeld@posteo.de>
2024-05-16 18:42:55 +0200

    all green but not all good

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :restaurant
   validates :restaurant, presence: true
   validates :content, :rating, presence: true
-  validates :rating, 
+  validates :rating, numericality: { only_integer: true }, inclusion: { in: 0..5, message: "must be between 0 and 5" }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Deleting all current restaurants..."
+Restaurant.destroy_all
+
+puts "Repopulating with new entries..."
+
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: 5, category: "italian"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: 5564684, category: "italian"}
+indiana_bones =  {name: "Indiana Bones", address: "5A Hügelweg, Haushausen", phone_number: 11246677, category: "belgian"}
+leckerei_brei =  {name: "Leckerei Brei", address: "Markstraße 23,Zossen", phone_number: 5668898, category: "french"}
+soba_noodles =  {name: "Soba Noodles", address: "Kantstraße 432, Belrin", phone_number: 11111115, category: "japanese"}
+
+[dishoom, pizza_east, indiana_bones, leckerei_brei, soba_noodles].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
